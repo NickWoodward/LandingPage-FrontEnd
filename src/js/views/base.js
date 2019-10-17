6db03ecfc297c999b08eb40ac67f3f00fee2d164
Nick Woodward <nick.woodward@hotmail.co.uk>
2019-10-17 20:44:26 +0100

    Design changes

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,7 +1,6 @@
 export const elements = {
     greeting: document.querySelector('.greeting'),
     tdl: document.querySelector('.todolist'),
-    tdlMsg: document.querySelector('.todolist__message'),
     tdlItems: document.querySelector('.todolist__items'),
     tdlForm: document.querySelector('.todolist__form'),
     tdlControls: document.querySelector('.todolist__nav'),
